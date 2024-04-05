b6e4400a4389c1e7a46fe7b2f0cd069894951cce
imon-sheikh <mdimonhossain2017@gmail.com>
2024-04-05 17:25:24 +0600

    work on readmefile

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link} from "react-router-dom";
 import { getBooks } from "../../utils";
 import ReadBooks from "../ReadBooks/ReadBooks";
 import WishlistBooks from "../WishlistBooks/WishlistBooks";
